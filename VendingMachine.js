cf1059900565902c4ba162de0e7685e2d0a61245
Brittany Hartmire and Frank Fernandez <bkhartmire+frank.m.fernandez@gmail.com>
2019-07-09 12:31:55 +0900

    done test 1

--- VendingMachine.js
+++ VendingMachine.js
@@ -4,3 +4,20 @@
   What is that? Well, glad you asked.
   Read about it here: https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 */
+
+class VendingMachine {
+  constructor() {
+    this.balance = 0;
+    this.till = { 10: 0, 50: 0, 100: 0, 500: 0 };
+  }
+  insertCoin(number) {
+    this.balance = number;
+    for (const key in this.till) {
+      if (JSON.stringify(number) === key) {
+        this.till[key]++;
+      }
+    }
+  }
+}
+
+module.exports = { VendingMachine };
--- test/test.js
+++ test/test.js
@@ -1,7 +1,17 @@
-const VendingMachine = require("../VendingMachine");
+const { VendingMachine } = require("../VendingMachine");
 const { expect } = require("chai");
 
 describe("vending machine", () => {
+  // const juice = {name: `Apple Juice`, price: 350, count: 5};
+  // const coffee = {name: "Tully's", price: 250, count: 7};
+
+  // const inventory = [
+  //   // [juice, coffee, ..., ...],
+  //   // [..., ..., ..., ...],
+  //   // [..., ..., ..., ...],
+  //   // [..., ..., ..., ...],
+  // ]
+
   it("should accept valid coins", () => {
     // Setup
     const machine = new VendingMachine();
