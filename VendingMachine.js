5e3e756efaa32c6d453ddd302d5a8092b7022157
Brittany Hartmire and Frank Fernandez <bkhartmire+frank.m.fernandez@gmail.com>
2019-07-09 16:44:45 +0900

    select row, selec column, start of addInventory

--- VendingMachine.js
+++ VendingMachine.js
@@ -9,6 +9,9 @@ class VendingMachine {
   constructor() {
     this.balance = 0;
     this.till = { 10: 0, 50: 0, 100: 0, 500: 0 };
+    this.selectedRow = null;
+    this.selectedColumn = null;
+    this.inventory = [];
   }
   insertCoin(number) {
     this.balance = number;
@@ -17,6 +20,21 @@ class VendingMachine {
         this.till[key]++;
       }
     }
+  }
+  selectRow(rowLetter) {
+    const validRows = ["A", "B", "C", "D"];
+    if (validRows.includes(rowLetter)) {
+      this.selectedRow = rowLetter;
+    }
+    console.log(this.selectedRow);
+  }
+
+  selectColumn(columnNumber) {
+    const validColumns = [1, 2, 3, 4];
+    if (validColumns.includes(columnNumber)) {
+      this.selectedColumn = columnNumber;
+    }
+    console.log(this.selectedColumn);
   }
 }
 
--- test/test.js
+++ test/test.js
@@ -2,17 +2,18 @@ const { VendingMachine } = require("../VendingMachine");
 const { expect } = require("chai");
 
 describe("vending machine", () => {
-  // const juice = {name: `Apple Juice`, price: 350, count: 5};
-  // const coffee = {name: "Tully's", price: 250, count: 7};
+  const juice = { name: `Apple Juice`, price: 350, count: 5 };
+  const coffee = { name: "Tully's", price: 250, count: 7 };
+  const jungleJuice = { name: `Jungle Juice`, price: 700, count: 10 };
+  const icedCoffee = { name: "Starbucks", price: 500, count: 2 };
 
   // const inventory = [
-  //   // [juice, coffee, ..., ...],
-  //   // [..., ..., ..., ...],
-  //   // [..., ..., ..., ...],
-  //   // [..., ..., ..., ...],
-  // ]
-
-  it("should accept valid coins", () => {
+  //   [juice, coffee, jungleJuice, icedCoffee],
+  //   [coffee, jungleJuice, icedCoffee, juice],
+  //   [jungleJuice, icedCoffee, juice, coffee],
+  //   [icedCoffee, juice, coffee, jungleJuice],
+  // ];
+  new it("should accept valid coins", () => {
     // Setup
     const machine = new VendingMachine();
 
@@ -28,4 +29,90 @@ describe("vending machine", () => {
     });
     expect(machine.balance).to.equal(500); // Use an ES6 getter
   });
+  it("should have a selectRow method that checks for valid row input", () => {
+    const machine = new VendingMachine();
+    expect(machine.selectedRow).to.equal(null);
+    machine.selectRow(201);
+    expect(machine.selectedRow).to.equal(null);
+    machine.selectRow("B");
+    expect(machine.selectedRow).to.equal("B");
+    machine.selectRow("Z");
+    expect(machine.selectedRow).to.equal("B");
+    machine.selectRow("A");
+    expect(machine.selectedRow).to.equal("A");
+  });
+  it("should have a selectColumn method that checks for valid column input", () => {
+    const machine = new VendingMachine();
+    expect(machine.selectedColumn).to.equal(null);
+    machine.selectColumn(201);
+    expect(machine.selectedColumn).to.equal(null);
+    machine.selectColumn(1);
+    expect(machine.selectedColumn).to.equal(1);
+    machine.selectColumn(5);
+    expect(machine.selectedColumn).to.equal(1);
+    machine.selectColumn(2);
+    expect(machine.selectedColumn).to.equal(2);
+  });
+  it("should have an add inventory method", () => {
+    const machine = new VendingMachine();
+    machine.addInventory(coffee);
+    machine.addInventory(coffee);
+    machine.addInventory(coffee);
+    machine.addInventory(coffee);
+
+    let expectedInventory = [[coffee, coffee, coffee, coffee], [], [], []];
+    expect(machine.inventory).to.deep.equal(expectedInventory);
+
+    machine.addInventory(juice);
+    machine.addInventory(juice);
+    machine.addInventory(juice);
+    machine.addInventory(juice);
+
+    expectedInventory = [
+      [coffee, coffee, coffee, coffee],
+      [juice, juice, juice, juice],
+      [],
+      [],
+    ];
+    expect(machine.inventory).to.deep.equal(expectedInventory);
+
+    machine.addInventory(icedCoffee);
+    machine.addInventory(icedCoffee);
+    machine.addInventory(icedCoffee);
+    machine.addInventory(icedCoffee);
+
+    expectedInventory = [
+      [coffee, coffee, coffee, coffee],
+      [juice, juice, juice, juice],
+      [icedCoffee, icedCoffee, icedCoffee, icedCoffee],
+      [],
+    ];
+
+    expect(machine.inventory).to.deep.equal(expectedInventory);
+
+    machine.addInventory(jungleJuice);
+    machine.addInventory(jungleJuice);
+    machine.addInventory(jungleJuice);
+    machine.addInventory(jungleJuice);
+
+    expectedInventory = [
+      [coffee, coffee, coffee, coffee],
+      [juice, juice, juice, juice],
+      [icedCoffee, icedCoffee, icedCoffee, icedCoffee],
+      [jungleJuice, jungleJuice, jungleJuice, jungleJuice],
+    ];
+
+    expect(machine.inventory).to.deep.equal(expectedInventory);
+
+    machine.addInventory(coffee);
+    expect(machine.inventory).to.deep.equal(expectedInventory);
+  });
+  // it("should check if the user put in enough money", () => {
+  //   const machine = new VendingMachine();
+  //   machine.insertCoin(500);
+  //   machine.selectRow('A');
+  //   machine.selectColumn(1);
+  //   expect(machine.checkIfEnoughMoney()).to.equal(true);
+
+  // });
 });
